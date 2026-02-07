0131dddaf8afc48e50d450d2cbd2a3aab03a1c45
Muneeb Mennad <mennadmuneeb@gmail.com>
2026-02-07 15:48:05 -0330

    Add appearance configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,7 +12,89 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 10
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 9.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+}
+
+config.colors = {
+  tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
+}
+
+-- Window padding styling
+config.window_padding = {
+  left = 3,
+  right = 3,
+  top = 0,
+  bottom = 1,
+}
+
+-- Inactive pane styling
+config.inactive_pane_hsb = {
+  saturation = 0.9,
+  brightness = 0.6,
+}
+
+-- Attach an image to the background of the wezterm window, uncomment below.
+-- config.window_background_image = '/path/to/wallpaper.jpg'
+
+-- config.window_background_image_hsb = {
+  -- brightness = 0.3,
+  -- hue = 1.0,
+  -- saturation = 1.0,
+-- }
+
+-- config.window_background_gradient = {
+--   colors = { '#1d2021', '#323027' },
+--   orientation = {
+--     Radial = {
+--       -- Specifies the x coordinate of the center of the circle,
+--       -- in the range 0.0 through 1.0.  The default is 0.5 which
+--       -- is centered in the X dimension.
+--       cx = 0.75,
+--
+--       -- Specifies the y coordinate of the center of the circle,
+--       -- in the range 0.0 through 1.0.  The default is 0.5 which
+--       -- is centered in the Y dimension.
+--       cy = 0.75,
+--
+--       -- Specifies the radius of the notional circle.
+--       -- The default is 0.5, which combined with the default cx
+--       -- and cy values places the circle in the center of the
+--       -- window, with the edges touching the window edges.
+--       -- Values larger than 1 are possible.
+--       radius = 1.25,
+--     },
+--   },
+-- }
+
+-- Background opacity
+config.window_background_opacity = 1.0
+
+-- Text background opacity
+config.text_background_opacity = 0.5
 
 -- Finally, return the configuration to wezterm:
 return config
