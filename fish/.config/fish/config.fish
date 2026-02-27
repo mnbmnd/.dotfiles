ecfd603dad186c3692efacff267f173ea27a8f8e
Muneeb Mennad <mennadmuneeb@gmail.com>
2026-02-27 07:19:14 -0330

    file structure udpate

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,26 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+set -x LANG en_CA.UTF-8
+set -x LC_ALL en_CA.UTF-8
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+fish_add_path ~/.local/bin
+
+function passman
+    python3 ~/projects/password-manager/main.py
+end
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
+# Aliases brahtato
+alias ssh='kitten ssh'
+alias comp4304="cd ~/COMP\ 4304; and source venv/bin/activate.fish"
